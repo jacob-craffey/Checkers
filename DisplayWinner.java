21bc58d8d1943900e82e3ca25e3ba1e4b3115432
nick-frein <freinn@mail.gvsu.edu>
2016-10-14 18:01:52 -0400

    Add files via upload
    Fixed checkstyle errors for all files.

--- DisplayWinner.java
+++ DisplayWinner.java
@@ -10,24 +10,25 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class DisplayWinner {
-	public static void display(String winner) {
+  
+  /** Creates a window that displays who won the game. **/
+  public static void display(String winner) {
 
-		JFrame frame3 = new JFrame("Winner");
-		frame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame3.setSize(150, 150);
-		frame3.setLocation(400, 200);
-		frame3.setLayout(new FlowLayout());
-		frame3.setVisible(true);
-		
-		JPanel panel3 = new JPanel();
-		panel3.setLayout(new FlowLayout());
-		frame3.add(panel3);
-		
-		JLabel label3 = new JLabel(winner);
-		JButton button3 = new JButton("OK");
-		
-		panel3.add(label3);
-		panel3.add(button3);
+    JFrame frame3 = new JFrame("Winner");
+    frame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame3.setSize(150, 150);
+    frame3.setLocation(400, 200);
+    frame3.setLayout(new FlowLayout());
+    frame3.setVisible(true);
 
-	}
+    JPanel panel3 = new JPanel();
+    panel3.setLayout(new FlowLayout());
+    frame3.add(panel3);
+
+    JLabel label3 = new JLabel(winner);
+    JButton button3 = new JButton("OK");
+
+    panel3.add(label3);
+    panel3.add(button3);
+  }
 }
--- StartGame.java
+++ StartGame.java
@@ -1,8 +1,8 @@
 package checkers;
 
 public class StartGame {
-	public static void main(String [] args) {
-		//DisplayWinner.display("Player 1 Wins!");  // test for DisplayWinner window
-		new ChooseColor(); // GUI is activated once colors are chosen
-	}
+  public static void main(String [] args) {
+    //DisplayWinner.display("Player 1 Wins!");  // test for DisplayWinner window
+    new ChooseColor(); // GUI is activated once colors are chosen
+  }
 }
